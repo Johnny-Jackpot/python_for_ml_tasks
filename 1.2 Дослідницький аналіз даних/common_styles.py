1d0041109ebb14a499ffabca21adf928f2f30042
alexandrn <alexandrn@default-value.com>
2024-05-03 00:28:18 +0300

    update notebook

--- 1.2 Дослідницький аналіз даних/common_styles.py
+++
@@ -1,6 +0,0 @@
-common_styles = [
-    {'selector': 'thead th', 'props': 'position: sticky; top:0; background-color:#000066 !important; color: white !important;'},
-    {'selector': 'tbody th', 'props': 'position: sticky; left:0; background-color:#000066 !important; color: white !important;'},
-    {'selector': 'tr:hover', 'props': 'background-color: lightblue; color: black'},
-    {'selector': 'td:hover', 'props': 'background-color: yellow; color: black'}
-]
