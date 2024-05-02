f98a659f0311ff46de85154a6e6941fcedcc555a
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-05-03 00:22:18 +0300

    added styles

---
+++ 1.2 Дослідницький аналіз даних/common_styles.py
@@ -0,0 +1,6 @@
+common_styles = [
+    {'selector': 'thead th', 'props': 'position: sticky; top:0; background-color:#000066 !important; color: white !important;'},
+    {'selector': 'tbody th', 'props': 'position: sticky; left:0; background-color:#000066 !important; color: white !important;'},
+    {'selector': 'tr:hover', 'props': 'background-color: lightblue; color: black'},
+    {'selector': 'td:hover', 'props': 'background-color: yellow; color: black'}
+]
